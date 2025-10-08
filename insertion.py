ce2d8526fc3a161dd7017eca8e009466a74f362a
Lucian516 <lucian.poljak@gmail.com>
2025-10-08 21:46:30 +0200

    Update insertion.py

--- insertion.py
+++ insertion.py
@@ -5,7 +5,7 @@ def insertion(arr):
     start = time.time()
     for i in range(len(arr)):
         for j in range(len(arr)):
-            if arr[i]>arr[j]:
+            if arr[i]<arr[j]:
                 arr[i], arr[j] = arr[j], arr[i]
             count+=1
 
